f08a34751b71c7c901dc7591c2a0cd9ce196d9c6
dormi_riso <leonimichelangelo17@gmail.com>
2025-10-13 16:52:20 +0200

    Refactor database connection and query methods for improved error handling and security

--- src/cage/db_manager.py
+++ src/cage/db_manager.py
@@ -1,33 +1,34 @@
 import sqlite3
 
 def connect_to_database(db_name="database/cage.db"):
-    '''Function to connect to the SQLite database
-    Returns a connection object'''
-    try:
-        con = sqlite3.connect(db_name)
-        return con
-    except ValueError:
-        print("Error connecting to the database")
-        return None
+    ''' Connect to the SQLite database specified by db_name. '''
+    con = sqlite3.connect(db_name)
+    return con
 
-def get_coordinates_from_crag_name(crag_name, db_name="database/cage.db"):
-    '''Function to get the coordinates of a crag from its name
-    Returns a tuple (latitude, longitude)'''
+def init_db(db_name="database/cage.db"):
+    ''' Initialize the database with the required tables. '''
     con = connect_to_database(db_name)
     cur = con.cursor()
-    res = cur.execute(f"SELECT lat, lon FROM crag WHERE name='{crag_name}';")
+    cur.execute("CREATE TABLE IF NOT EXISTS crag(name TEXT PRIMARY KEY, lat REAL, lon REAL)")
+    con.commit()
+    con.close()
+
+def get_coordinates_from_crag_name(crag_name, db_name="database/cage.db"):
+    ''' Retrieve the latitude and longitude of a crag given its name. 
+    Returns a tuple (lat, lon) or None if the crag is not found.
+    '''
+    con = connect_to_database(db_name)
+    cur = con.cursor()
+    res = cur.execute("SELECT lat, lon FROM crag WHERE name = ?", (crag_name,))
     coordinates = res.fetchone()
     con.close()
     return coordinates
 
 def insert_new_crag(crag_name: str, lat: float, lon: float, db_name="database/cage.db"):
-    '''Function to insert a new crag into the database
-    Returns True if the insertion was successful'''
+    ''' Insert a new crag into the database. Returns True if successful. '''
     con = connect_to_database(db_name)
     cur = con.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS crag(name TEXT, lat REAL, lon REAL)")
-    con.commit()
-    cur.execute(f"""INSERT INTO crag (name, lat, lon) VALUES ('{crag_name}', {lat}, {lon})""")
+    cur.execute("INSERT INTO crag (name, lat, lon) VALUES (?, ?, ?)", (crag_name, lat, lon))
     con.commit()
     con.close()
     return True
